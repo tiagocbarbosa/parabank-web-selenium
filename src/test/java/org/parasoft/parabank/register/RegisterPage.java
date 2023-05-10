c9639301f410fd8f1b801d03ce401d50e43e9d0e
tiagocbarbosa <tiagocastrobarbosa@gmail.com>
2023-05-09 21:54:30 -0300

    test created

--- src/test/java/org/parasoft/parabank/register/RegisterPage.java
+++ src/test/java/org/parasoft/parabank/register/RegisterPage.java
@@ -1,11 +1,47 @@
 package org.parasoft.parabank.register;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.parasoft.parabank.PageObject;
 
 public class RegisterPage extends PageObject {
 
+    private static String username;
+
     public RegisterPage(WebDriver browser) {
         super(browser);
+    }
+
+    public void fillTheRegisterForm(String firstName, String lastName, String street, String city, String state, String zipCode, String phoneNumber, String ssn, String username, String password) {
+        RegisterPage.username = username;
+
+        this.browser.findElement(By.id("customer.firstName")).sendKeys(firstName);
+        this.browser.findElement(By.id("customer.lastName")).sendKeys(lastName);
+        this.browser.findElement(By.id("customer.address.street")).sendKeys(street);
+        this.browser.findElement(By.id("customer.address.city")).sendKeys(city);
+        this.browser.findElement(By.id("customer.address.state")).sendKeys(state);
+        this.browser.findElement(By.id("customer.address.zipCode")).sendKeys(zipCode);
+        this.browser.findElement(By.id("customer.phoneNumber")).sendKeys(phoneNumber);
+        this.browser.findElement(By.id("customer.ssn")).sendKeys(ssn);
+        this.browser.findElement(By.id("customer.username")).sendKeys(RegisterPage.username);
+        this.browser.findElement(By.id("customer.password")).sendKeys(password);
+        this.browser.findElement(By.id("repeatedPassword")).sendKeys(password);
+    }
+
+    public void registerAccount() {
+        this.browser.findElement(By.xpath("//input[contains(@value,'Register')]")).click();
+    }
+
+    public boolean isAccountRegistered() {
+        return this.browser.findElement(By.xpath("//h1[@class='title'][contains(.,'Welcome " + RegisterPage.username + "')]")).isDisplayed();
+    }
+
+    public boolean isRegisteredMessageDisplayed() {
+        return this.browser.findElement(By.xpath("//p[contains(.,'Your account was created successfully. You are now logged in.')]")).isDisplayed();
+    }
+
+    public boolean isLogOutButtonDisplayed() {
+        return this.browser.findElement(By.xpath("//a[contains(.,'Log Out')]")).isDisplayed();
     }
 }
--- src/test/java/org/parasoft/parabank/register/RegisterTest.java
+++ src/test/java/org/parasoft/parabank/register/RegisterTest.java
@@ -1,14 +1,45 @@
 package org.parasoft.parabank.register;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.parasoft.parabank.home.HomePage;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class RegisterTest {
+
+    private RegisterPage registerPage;
+
+    @AfterEach
+    public void afterEach() {
+        this.registerPage.close();
+    }
 
     @Test
     public void shouldRegisterAccountWithSuccess() {
         HomePage homePage = new HomePage();
-        RegisterPage registerPage = homePage.registerNewCustomer();
-        registerPage.close();
+        this.registerPage = homePage.navigateToRegisterPage();
+
+        String firstName = "Fulano";
+        String lastName = "da Silva";
+        String street = "Street";
+        String city = "City";
+        String state = "State";
+        String zipCode = "40000000";
+        String phoneNumber = "123456789";
+        String ssn = "123456";
+        String todayDateAndTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyHHmmss"));
+        String username = "fulano" + todayDateAndTime;
+        String password = "123";
+        this.registerPage.fillTheRegisterForm(firstName, lastName, street, city, state, zipCode, phoneNumber, ssn,
+                username, password);
+        this.registerPage.registerAccount();
+
+        Assertions.assertTrue(this.registerPage.isAccountRegistered());
+        Assertions.assertTrue(this.registerPage.isRegisteredMessageDisplayed());
+        Assertions.assertTrue(this.registerPage.isLogOutButtonDisplayed());
     }
 }
