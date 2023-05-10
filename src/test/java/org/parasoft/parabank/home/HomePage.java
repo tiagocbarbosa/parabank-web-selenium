dd839f7fdaa2dd99e510c2e5599bbfc3b0be7849
tiagocbarbosa <tiagocastrobarbosa@gmail.com>
2023-05-10 00:30:50 -0300

    method created

--- src/test/java/org/parasoft/parabank/home/HomePage.java
+++ src/test/java/org/parasoft/parabank/home/HomePage.java
@@ -2,6 +2,7 @@ package org.parasoft.parabank.home;
 
 import org.openqa.selenium.By;
 import org.parasoft.parabank.PageObject;
+import org.parasoft.parabank.contact.ContactPage;
 import org.parasoft.parabank.register.RegisterPage;
 
 public class HomePage extends PageObject {
@@ -16,5 +17,10 @@ public class HomePage extends PageObject {
     public RegisterPage navigateToRegisterPage() {
         browser.findElement(By.xpath("//a[contains(.,'Register')]")).click();
         return new RegisterPage(browser);
+    }
+
+    public ContactPage navigateToContactPage() {
+        browser.findElement(By.xpath("//a[contains(.,'contact')]")).click();
+        return new ContactPage(browser);
     }
 }
