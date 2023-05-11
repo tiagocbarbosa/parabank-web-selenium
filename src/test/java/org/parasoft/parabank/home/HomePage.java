bcab8129dcbebfaefa28888c631fad1452574677
tiagocbarbosa <tiagocastrobarbosa@gmail.com>
2023-05-10 23:16:22 -0300

    param added; methods created

--- src/test/java/org/parasoft/parabank/home/HomePage.java
+++ src/test/java/org/parasoft/parabank/home/HomePage.java
@@ -1,17 +1,19 @@
 package org.parasoft.parabank.home;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.parasoft.parabank.PageObject;
 import org.parasoft.parabank.contact.ContactPage;
+import org.parasoft.parabank.overview.OverviewPage;
 import org.parasoft.parabank.register.RegisterPage;
 
 public class HomePage extends PageObject {
 
     public static final String URL_HOME = "https://parabank.parasoft.com/parabank/index.htm";
 
-    public HomePage() {
-        super(null);
-        browser.navigate().to(URL_HOME);
+    public HomePage(WebDriver browser) {
+        super(browser);
+        this.browser.navigate().to(URL_HOME);
     }
 
     public RegisterPage navigateToRegisterPage() {
@@ -22,5 +24,15 @@ public class HomePage extends PageObject {
     public ContactPage navigateToContactPage() {
         browser.findElement(By.xpath("//a[contains(.,'contact')]")).click();
         return new ContactPage(browser);
+    }
+
+    public void fillLoginForm(String username, String password) {
+        this.browser.findElement(By.xpath("//input[contains(@type,'text')]")).sendKeys(username);
+        this.browser.findElement(By.xpath("//input[contains(@type,'password')]")).sendKeys(password);
+    }
+
+    public OverviewPage login() {
+        this.browser.findElement(By.xpath("//input[contains(@type,'password')]")).submit();
+        return new OverviewPage(this.browser);
     }
 }
